46d08e346468037fc1ab6e075af5f102f7c6a77a
krepak <krepakvitaliy@gmail.com>
2019-10-23 12:57:13 +0300

    [sim] added sim files

---
+++ soft/Inc/plc_module.h
@@ -0,0 +1,11 @@
+/**
+  ******************************************************************************
+  * @file    plc_module.h
+  * @author  zorroxied
+  * @version V1.0.0
+  * @date    10.10.2019
+  * @brief   
+  *          Data Container for the Module
+  *          PLC Module XS Rev 1.2 (plc_module_xs_Rev1.2_PWM_Outputs-3)
+  ******************************************************************************
+  */
---
+++ soft/Src/plc_module.c
@@ -0,0 +1,13 @@
+/**
+  ******************************************************************************
+  * @file    plc_module.c
+  * @author  zorroxied
+  * @version V1.0.0
+  * @date    10.10.2019
+  * @brief   
+  *          Data Container for the Module
+  *          PLC Module XS Rev 1.2 (plc_module_xs_Rev1.2_PWM_Outputs-3)
+  ******************************************************************************
+  */
+  
+  struct 
